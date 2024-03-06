3664184f6b5c6fc73bcfad89399cb775d5769781
Yelyzaveta Shydenko <yelyzavetashydenko@MacBook-Pro-Kostiantyn.local>
2024-03-06 23:47:04 +0100

    add homework

---
+++ Homework 06.03/script.js
@@ -0,0 +1,46 @@
+// 1. Напишите функцию, которая использует forEach для умножения каждого элемента массива на 2 и вывода результата в консоль.
+
+// const numbers = [2, 4, 6, 1, 3, 6, 24, 61, 36, 46, 13 ]
+
+// function multiplyByTwo(array) {
+//     array.forEach(function(elem) {
+//         console.log(elem * 2);
+//     })
+// }
+
+// multiplyByTwo(numbers)
+
+// 2.Напишите функцию, которая использует forEach, чтобы складывать все элементы массива и возвращать сумму.
+
+// const numbers = [2, 4, 6, 1, 3, 6, 24, 61, 36, 46, 13 ]
+
+// function sumOfElements(array){
+//     let sum = 0
+//     array.forEach(elem => {
+//        sum += elem;  
+//     });
+//     console.log(sum); 
+// }
+// sumOfElements(numbers)
+
+// 3.Дан массив строк. Используйте forEach, чтобы преобразовать каждую строку в верхний регистр и сохранить изменения в том же массиве.
+// const words = ['There', 'was', 'a', 'nine', 'tailed', 'red', 'fox']
+
+// function wordsUpperCase(arrays) {
+//     arrays.forEach(function(elem, index, array){
+//         array[index] = elem.toUpperCase();
+//     });
+// }
+
+// wordsUpperCase(words)
+// console.log(words);
+
+// На map
+// 4.Дан массив чисел. Создайте новый массив, в котором будут лежать квадраты каждого числа из исходного массива.
+
+const words = ['There', 'was', 'a', 'nine', 'tailed', 'red', 'fox']
+
+
+// 5.Дан массив объектов с информацией о товарах. Необходимо создать новый массив, содержащий только имена товаров.
+
+// 6.Дан массив чисел. Создайте новый массив, в котором будут только отрицательные числа из исходного массива.
