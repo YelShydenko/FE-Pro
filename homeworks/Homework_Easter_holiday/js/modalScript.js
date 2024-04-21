cfab3359e37a77522a0d2387ca3b1c6800739d67
Yelyzaveta Shydenko <lizawinsdale@gmail.com>
2024-04-21 19:28:07 +0200

    add homework 21.04

--- homeworks/Homework_Easter_holiday/js/modalScript.js
+++ homeworks/Homework_Easter_holiday/js/modalScript.js
@@ -44,7 +44,7 @@ function createPost(data) {
   const modal = document.createElement("div");
   modal.classList.add("modal");
 
-  const closeModal = document.createElement('div')
+  const closeModal = document.createElement("div");
   closeModal.classList.add("close__modal");
 
   const modalTitle = document.createElement("h2");
@@ -89,20 +89,111 @@ function createPost(data) {
   modalBackground.append(modal);
 
   const modalPlace = document.querySelector(".place__for__modal");
-  modalPlace.append(modalBackground)
+  modalPlace.append(modalBackground);
 
   // document.body.append(modalBackground);
 
-  closeModal.addEventListener('click', function() {
-    modalBackground.remove()
-  })
-
+  closeModal.addEventListener("click", function () {
+    modalBackground.remove();
+  });
 }
 
 const buttonFetch = document.querySelector("#btn");
 
-
-
 buttonFetch.addEventListener("click", function () {
   getPosts();
 });
+
+// -------------------- Домашка 17.04 --------------------------------------------------------------------------------------------------------
+// Создание класса Employee: Создайте класс Employee с конструктором, который принимает три параметра: name, age, position. Конструктор должен инициализировать соответствующие свойства объекта.
+// Добавление метода getInfo(): Добавьте в класс Employee метод getInfo(), который возвращает строку с информацией о сотруднике в формате "Имя: Возраст, Должность".
+// Фильтрация сотрудников по должности:
+// Напишите функцию filterEmployeesByPosition, которая принимает два параметра: список сотрудников (employees) и должность (position).
+// Отфильтруйте список сотрудников по заданной должности.
+// Используйте метод map, чтобы преобразовать отфильтрованный список в список имен сотрудников.
+// Функция должна возвращать массив имен сотрудников с заданной должностью.
+// Создайте несколько объектов сотрудников, используя класс Employee.
+// Используйте функцию filterEmployeesByPosition для фильтрации сотрудников по их должности.
+// Результат фильтрации в вывести на страницу во втором модальном окне на вашем сайте pallas cat studio. результат выложить на гитхаб в прошлый проект, но разделите новый код комментариями!!
+
+class Employee {
+  constructor(name, age, position) {
+    this.name = name;
+    this.age = age;
+    this.position = position;
+  }
+
+  getinfo() {
+    return `Имя сотрудника: ${this.name}, возраст: ${this.age}, должность: ${this.position}`;
+  }
+}
+
+const employees = [
+  new Employee("Yelyzaveta Shydenko", 21, "Front-end Developer"),
+  new Employee("Johnny Depp", 35, "Front-end Developer"),
+  new Employee("Kostiantyn Karpenko", 27, "Full-stack Developer"),
+  new Employee("Noah Muller", 33, "Full-stack Developer"),
+  new Employee("Dmitrii Gostevskii", 31, "Back-end Developer"),
+  new Employee("Rick Sanches", 55, "Back-end Developer"),
+  new Employee("Maria Kukhar", 21, "UI/UX Designer"),
+  new Employee("Morty Smith", 15, "UI/UX Designer"),
+  new Employee("Daniil Martynov", 28, "Android Developer"),
+  new Employee("Sam Winchester", 23, "Android Developer"),
+];
+
+function filterEmployeesByPosition(employees, position) {
+  const filteredEmployees = employees.filter((employee) => {
+    return employee.position === position;
+  });
+  const names = filteredEmployees.map(function (employee) {
+    return employee.name;
+  });
+  return names;
+}
+
+const buttonEmployee = document.querySelector("#employee__btn");
+
+buttonEmployee.addEventListener("click", function () {
+  const filteredNames = filterEmployeesByPosition(
+    employees,
+    "Front-end Developer"
+  );
+  // console.log(filteredNames);
+  createModalEmployee(filteredNames);
+});
+
+function createModalEmployee(filteredNames) {
+  const modal = document.createElement("div");
+  modal.classList.add("modal");
+
+  const closeModal = document.createElement("div");
+  closeModal.classList.add("close__modal");
+
+  const modalTitle = document.createElement("h2");
+  modalTitle.textContent = "Employee info";
+
+  const modalContent = document.createElement("div");
+  modalContent.classList.add("modal__content");
+
+  const nameList = document.createElement("ul");
+
+  filteredNames.forEach((name) => {
+    const listName = document.createElement("li");
+    listName.textContent = name;
+    nameList.append(listName);
+  });
+  modalContent.append(nameList);
+  modal.append(closeModal, modalTitle, modalContent);
+
+  const modalBackground = document.createElement("div");
+  modalBackground.classList.add("modal__background");
+
+  modalBackground.append(modal);
+
+  const modalPlace = document.querySelector(".place__for__modal");
+  modalPlace.append(modalBackground);
+
+  closeModal.addEventListener("click", function () {
+    modalBackground.remove();
+  });
+}
