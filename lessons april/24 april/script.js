46667092b30d968e715716282ba037ab65805880
Yelyzaveta Shydenko <lizawinsdale@gmail.com>
2024-04-24 12:53:32 +0200

    add lesson 24.04

--- lessons april/24 april/script.js
+++ lessons april/24 april/script.js
@@ -1,1 +1,5 @@
-// 
+const burger = document.querySelector(".burger");
+const headerMobile = document.querySelector(".header__mobile");
+burger.addEventListener("click", () => {
+  headerMobile.classList.toggle("active");
+});
